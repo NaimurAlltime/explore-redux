c64d73d894634b0820caf36f6d9c119ea5a7becb
Naimur Rahman <naimur2231@gmail.com>
2023-08-23 00:47:24 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2 className="text-3xl font-semibold">Explore Redux</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;

3d928198e66a86b997283c95ee48906155c4d621
Naimur Rahman <naimur2231@gmail.com>
2023-08-23 01:40:45 +0600

    redux first slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <div>
-        <h2 className="text-3xl font-semibold">Explore Redux</h2>
-      </div>
+      <Home></Home>
     </>
   );
 }
---
+++ src/components/counter.jsx
@@ -0,0 +1,9 @@
+const counter = () => {
+  return (
+    <div>
+      <h2>Counter</h2>
+    </div>
+  );
+};
+
+export default counter;
